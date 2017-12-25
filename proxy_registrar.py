d1ca008f988a8bbc280044bdfd3bcdc6a59be14e
monicagomezavila <monicagomezavila@github.com>
2017-12-25 22:40:21 +0100

    Añadido en def handle que mire si el mensaje decodificado del cliente es REGISTER o REGISTER con autenticación

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -47,15 +47,23 @@ class Proxy_Registrar(socketserver.DatagramRequestHandler):
         Ldata = cts.PrReg()
         self.prip = Ldata[0]
         self.prport = Ldata[1]
-        print(self.prip)
-        print(self.prport)
 
         # Parte de función servidora.
+        lines = []
         for line in self.rfile:
-            if line.decode('utf-8') != '\r\n' or '' or not line:
-                linea = line.decode('utf-8')
-                print(linea)
+            if not line or line.decode('utf-8') != '\r\n':
+                line = line.decode('utf-8')
+                print(line)
+                lines.append(line)
 
+        METHOD = (lines[0][:lines[0].find(' ')])
+        if METHOD == 'REGISTER' and len(lines) == 2:
+            message = 'SIP/2.0 401 Unauthorized\r\n'
+            message += 'WWW Authenticated: Digest nonce="898989"\r\n\r\n'
+            self.wfile.write(bytes(message, 'utf-8'))
+
+        elif METHOD == 'REGISTER' and len(lines) == 3:
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     """
@@ -69,11 +77,11 @@ if __name__ == "__main__":
     try:
         dproxy = sys.argv[1]
     except IndexError:
-        sys.exit('Usage: python uaclient.py config method option')
+        sys.exit('Usage: python proxy_registrar.py config')
     try:
         parser.parse(open(dproxy))
     except FileNotFoundError:
-        sys.exit('Usage: python uaclient.py config method option')
+        sys.exit('Usage: python proxy_registrar.py config')
 
     Ldata = cts.PrReg()
     IP = repr(Ldata[0])
