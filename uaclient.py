06fe54fbf1b287000319ef8ee7f40a77a165b72f
monicagomezavila <monicagomezavila@github.com>
2017-12-25 22:30:18 +0100

    Modificado def Date y añadido en def Method el mensaje que envia el cliente para autenticarse

--- uaclient.py
+++ uaclient.py
@@ -4,8 +4,11 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
+import os
 import os.path
 import time
+import socket
+import shutil
 
 
 class UaClient(ContentHandler):
@@ -14,7 +17,7 @@ class UaClient(ContentHandler):
     """
     def __init__(self):
         """
-        Variables que obtendremos del documento XML
+        Constantes que declaramos.
         """
         self.prip = ""
         self.prport = ""
@@ -22,41 +25,52 @@ class UaClient(ContentHandler):
         self.sport = ""
         self.rtpip = ""
         self.logpath = ""
+        self.sip = ""
+        self.uapasswd = ""
+        self.log = ""
+        self.logpath = ""
 
     def startElement(self, name, attrs):
         """
-        Método que lee etiquetas y guarda su valor
+        Método que lee etiquetas y guarda el atributo del documento xml.
         """
         if name == 'regproxy':
             self.prip = attrs.get('ip', "")
             self.prport = attrs.get('puerto', "")
         elif name == 'account':
             self.uaname = attrs.get('username', "")
+            self.uapasswd = attrs.get('passwd', "")
         elif name == 'uaserver':
             self.sport = attrs.get('puerto', "")
             self.sip = attrs.get('ip', "")
         elif name == 'rtpaudio':
             self.rtpport = attrs.get('puerto', "")
         elif name == 'log':
-            logpath = attrs.get('path', "")
-            self.log = logpath[logpath.rfind('/')+1:]
+            self.logpath = attrs.get('path', "")
+            self.log = self.logpath[self.logpath.rfind('/')+1:]
 
-    def Date(self, line):
-        if os.path.exists(self.log):
+    def Date(self, line, path):
+        """
+        Da la fecha del momento en el que se llama a la funcion.
+        Escribe en el fichero log.
+        """
+        if os.path.exists(path):
             l_date = (list(time.localtime(time.time()))[:6])
             date = ""
             for element in l_date:
                 date = date + str(element)
             date = date + (' ') + line + ('\n')
-            with open(self.log, 'a') as outfile:
+            with open(path, 'a') as outfile:
                 outfile.write(date)
 
     def Method(self):
         """
-        Parte de cliente. Se registra, manda invite, bye
+        Parte de cliente. Se registra, manda invite, bye.
         """
         if len(sys.argv) != 4:
             sys.exit('Usage: python uaclient.py config method option')
+
+        UaClient().Date('Starting...', self.logpath)
 
         if sys.argv[2] == 'REGISTER':
             line = 'REGISTER sip:' + self.uaname + ':'
@@ -64,21 +78,60 @@ class UaClient(ContentHandler):
             line += 'Expires: ' + sys.argv[3] + '\r\n\r\n'
         elif sys.argv[2] == 'INVITE':
             line = 'INVITE sip:' + sys.argv[3] + ' SIP/2.0\r\n'
-            line += 'Content-Type: application/sdp' + '\r\n\r\n'
+            line += 'Content-Type: application/sdp' + '\r\n'
             line += 'v=0\r\n' + 'o=' + self.uaname + ' ' + self.sip + '\r\n'
             line += 's=misesion\r\n' + 't=0\r\n'
-            line += 'm=audio ' + self.rtpport + ' RTP\r\n\r\n'
+            line += 'm=audio ' + self.rtpport + ' RTP\r\n'
         elif sys.argv[2] == 'BYE':
-            line = 'BYE sip:' + sys.argv[3] + ' SIP/2.0\r\n\r\n'
+            line = 'BYE sip:' + sys.argv[3] + ' SIP/2.0\r\n'
         else:
             print('Usage: python uaclient.py config method option')
 
-        print(line)
+        l_log = 'Sent to ' + self.prip + ':' + self.prport + (': ')
+        l_log += line[:line.rfind('\r\n\r\n')]
+        UaClient().Date(l_log, self.logpath)
+
+        """
+        Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+            my_socket.connect((str(self.prip), int(self.prport)))
+            my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+
+            data = my_socket.recv(1024)
+            print(data.decode('utf-8'))
+
+            l_log = 'Reviced from ' + self.prip + ':' + self.prport + (': ')
+            recived = data.decode('utf-8')
+            l_log += recived[:recived.rfind('\r\n\r\n')]
+            UaClient().Date(l_log, self.logpath)
+
+            message_proxy = (data.decode('utf-8').split())
+            if '401' in message_proxy:
+                line = 'REGISTER sip:' + self.uaname + ':'
+                line += self.sport + ' SIP/2.0\r\n'
+                line += 'Expires: ' + sys.argv[3] + '\r\n'
+                line += 'Authorization: Digest response="'
+                line += self.uapasswd + ('"\r\n')
+                my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+
+                l_log = 'Sent to ' + self.prip + ':' + self.prport + (': ')
+                l_log += line[:line.rfind('\r\n\r\n')]
+                UaClient().Date(l_log, self.logpath)
+
+                data = my_socket.recv(1024)
+                print(data.decode('utf-8'))
+                l_log = 'Reviced from ' + self.prip + ':' + self.prport
+                recived = data.decode('utf-8')
+                l_log += (': ') + recived[:recived.rfind('\r\n\r\n')] 
+                UaClient().Date(l_log, self.logpath)
+
 
 if __name__ == "__main__":
     """
     Programa principal
     """
+    # Coge las constantes de la clase UaClient
     parser = make_parser()
     client = UaClient()
     parser.setContentHandler(client)
@@ -88,5 +141,5 @@ if __name__ == "__main__":
         parser.parse(open(duser))
     except FileNotFoundError:
         sys.exit('Usage: python uaclient.py config method option')
+
     client.Method()
-    client.Date('hey')
