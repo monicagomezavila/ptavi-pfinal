a2da86412ac46ab18654f125764ae99a859443d1
monicagomezavila <monicagomezavila@github.com>
2018-01-02 14:36:36 +0100

    Realiza toda la parte del INVITE con el proxy y el uaclient, hasta el ACK. RTP todavia no implementado

--- uaserver.py
+++ uaserver.py
@@ -17,6 +17,8 @@ class UaServer(ContentHandler):
         self.log = ""
         self.sport = ""
         self.sip = ""
+        self.uaname = ""
+        self.rtpport = ""
 
     def startElement(self, name, attrs):
         """
@@ -27,13 +29,23 @@ class UaServer(ContentHandler):
         elif name == 'uaserver':
             self.sip = attrs.get('ip', "")
             self.sport = attrs.get('puerto', "")
+        elif name == 'account':
+            self.uaname = attrs.get('username', "")
+        elif name == 'rtpaudio':
+            self.rtpport = attrs.get('puerto', "")
+        elif name == 'regproxy':
+            self.prip = attrs.get('ip', "")
+            self.prport = attrs.get('puerto', "")
 
     def Return(self):
-        return(self.sip, self.log, self.sport)
+        return(self.sip, self.log, self.sport, self.uaname, self.rtpport,
+               self.prip, self.prport)
+
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    
+
     def handle(self):
+        defclient = uaclient.UaClient()
         parser = make_parser()
         server = UaServer()
         parser.setContentHandler(server)
@@ -41,14 +53,18 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         try:
             parser.parse(open(dua))
         except FileNotFoundError:
-             sys.exit('Usage: python uaclient.py config')
+            sys.exit('Usage: python uaclient.py config')
 
         Ldata = server.Return()
         self.log = Ldata[1]
         self.sport = Ldata[2]
         self.sip = Ldata[0]
+        self.uaname = Ldata[3]
+        self.rtpport = Ldata[4]
+        self.prip = Ldata[5]
+        self.prport = Ldata[6]
 
-        lines =[]
+        lines = []
         for line in self.rfile:
 
             if not line or line.decode('utf-8') != '\r\n':
@@ -56,11 +72,38 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 print(line)
                 lines.append(line)
 
+        if 'INVITE'in lines[0]:
+
+            p_log = 'Received from ' + self.prip + (':') + str(self.prport)
+            p_log += (' ') + lines[0].replace('\r\n', ' ')
+            defclient.Date((p_log), self.log)
+
+            line = "SIP/2.0 100 Trying\r\n\r\n"
+            line += "SIP/2.0 180 Ringing\r\n\r\n"
+
+            line += "SIP/2.0 200 OK\r\n"
+            line += 'Content-Type: application/sdp' + '\r\n\r\n'
+            line += 'v=0\r\n' + 'o=' + self.uaname + ' ' + self.sip + '\r\n'
+            line += 's=misesion\r\n' + 't=0\r\n'
+            line += 'm=audio ' + self.rtpport + ' RTP\r\n\r\n'
+            self.wfile.write(bytes(line, 'utf-8'))
+
+            l_log = 'Sent to ' + self.prip + (':') + self.prport + (' ')
+            l_log += "SIP/2.0 100 Trying " + "SIP/2.0 180 Ringing "
+            l_log += line.replace('\r\n', ' ')
+            defclient.Date((l_log), self.log)
+
+        if 'ACK' in lines[0]:
+            p_log = 'Received from ' + self.prip + (':') + str(self.prport)
+            p_log += (' ') + lines[0].replace('\r\n', ' ')
+            defclient.Date((p_log), self.log)
+
 if __name__ == "__main__":
 
     parser = make_parser()
     server = UaServer()
     parser.setContentHandler(server)
+    defclient = uaclient.UaClient()
     try:
         dproxy = sys.argv[1]
     except IndexError:
@@ -75,9 +118,12 @@ if __name__ == "__main__":
     IP = IP[:IP.rfind("'")]
     IP = IP[IP.find("'")+1:]
     PORT = Data[2]
+    LOG = Data[1]
     serv = socketserver.UDPServer((IP, int(PORT)), EchoHandler)
     print("Listening...")
+    defclient.Date(('Starting...'), LOG)
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
+        defclient.Date(('Finishing...'), LOG)
